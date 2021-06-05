f980ede790efab2e9f33c59be2e02e0a69a15399
Oskakim <o2kim0225@naver.com>
2021-06-05 21:23:42 +0900

    add permision

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import {Alert} from 'react-native';
 import Loading from './Loading';
 import * as Location from 'expo-location';
 
 export default class extends React.Component {
   getLocation = async() => {
-    const location = await Location.getCurrentPositionAsync(options);
-    console.log(locationn);
+    try{
+      await Location.requestForegroundPermissionsAsync();
+      const location = await Location.getCurrentPositionAsync();
+      console.log(location);
+    }
+    catch(error){
+      Alert.alert("error","permission");
+    }
   }
   componentDidMount(){
     this.getLocation();
