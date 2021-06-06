265831604171f3f23007866d2e1870a73755aa59
Oskakim <o2kim0225@naver.com>
2021-06-06 22:43:57 +0900

    rename async function

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import {Alert} from 'react-native';
 import Loading from './Loading';
 import * as Location from 'expo-location';
 import axios from 'axios';
+
 // https://home.openweathermap.org/api_keys
 const API_KEY = "125dcfdaf3aafea1b4ff50185c299681";
 
@@ -10,27 +11,31 @@ export default class extends React.Component {
   state = {
     isLoading: true
   };
-  getWeather = async(latitude, longitude) => {
+
+  getWeatherAsync = async(latitude, longitude) => {
     const { data } =
     await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`);
     console.log(data);
   };
-  getLocation = async() => {
+
+  getLocationAsync = async() => {
     try{
       await Location.requestForegroundPermissionsAsync();
       const {
         coords: {latitude, longitude}
       } = await Location.getCurrentPositionAsync();
-      this.getWeather(latitude, longitude);
+      this.getWeatherAsync(latitude, longitude);
       this.setState({ isLoading: false });
     }
     catch(error){
-      Alert.alert("error","permission");
+      Alert.alert("error", "check permission");
     }
   };
+
   componentDidMount(){
-    this.getLocation();
+    this.getLocationAsync();
   }
+  
   render(){
     const { isLoading } = this.state;
     return isLoading ? <Loading /> : null;
