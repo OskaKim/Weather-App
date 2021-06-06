31e9d2756f3e061e0ec15f75cf7f1abbbfb361d0
Oskakim <o2kim0225@naver.com>
2021-06-06 22:06:34 +0900

    sload end when get position info

--- App.js
+++ App.js
@@ -4,20 +4,26 @@ import Loading from './Loading';
 import * as Location from 'expo-location';
 
 export default class extends React.Component {
+  state = {
+    isLoading: true
+  };
   getLocation = async() => {
     try{
       await Location.requestForegroundPermissionsAsync();
-      const location = await Location.getCurrentPositionAsync();
-      console.log(location);
+      const {
+        coords: {latitude, longitude}
+      } = await Location.getCurrentPositionAsync();
+      this.setState({ isLoading: false });
     }
     catch(error){
       Alert.alert("error","permission");
     }
-  }
+  };
   componentDidMount(){
     this.getLocation();
   }
   render(){
-    return <Loading />
+    const { isLoading } = this.state;
+    return isLoading ? <Loading /> : null;
   }
 }
