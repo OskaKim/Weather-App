03646c19eef3c2d87abfea504a9c7c3dd845109d
Oskakim <o2kim0225@naver.com>
2021-06-07 22:20:29 +0900

    impl - show temperature

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import {Alert} from 'react-native';
+import { Alert } from 'react-native';
 import Loading from './Loading';
 import * as Location from 'expo-location';
 import axios from 'axios';
+import Weather from './Weather';
 
 // https://home.openweathermap.org/api_keys
 const API_KEY = "125dcfdaf3aafea1b4ff50185c299681";
@@ -12,32 +13,32 @@ export default class extends React.Component {
     isLoading: true
   };
 
-  getWeatherAsync = async(latitude, longitude) => {
+  getWeatherAsync = async (latitude, longitude) => {
     const { data } =
-    await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`);
-    console.log(data);
+      await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`);
+    this.setState({ isLoading: false, temp: data.main.temp });
   };
 
-  getLocationAsync = async() => {
-    try{
+  getLocationAsync = async () => {
+    try {
       await Location.requestForegroundPermissionsAsync();
       const {
-        coords: {latitude, longitude}
+        coords: { latitude, longitude }
       } = await Location.getCurrentPositionAsync();
       this.getWeatherAsync(latitude, longitude);
       this.setState({ isLoading: false });
     }
-    catch(error){
+    catch (error) {
       Alert.alert("error", "check permission");
     }
   };
 
-  componentDidMount(){
+  componentDidMount() {
     this.getLocationAsync();
   }
-  
-  render(){
-    const { isLoading } = this.state;
-    return isLoading ? <Loading /> : null;
+
+  render() {
+    const { isLoading, temp } = this.state;
+    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} />;
   }
 }
